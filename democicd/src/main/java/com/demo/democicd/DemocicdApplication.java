a19fea7798a5d7ae9724c3e8ae3b83a51601b643
Ankit Manglik <amanglik2010@gmail.com>
2025-02-03 16:34:31 +0530

    Update DemocicdApplication.java

--- democicd/src/main/java/com/demo/democicd/DemocicdApplication.java
+++ democicd/src/main/java/com/demo/democicd/DemocicdApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemocicdApplication {
 
 	public static void main(String[] args) {
+		System.out.println("changes added");
 		SpringApplication.run(DemocicdApplication.class, args);
 	}
 
